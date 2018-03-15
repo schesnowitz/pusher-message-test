3ed0a84842ae93ec80c5cffdd21d37b4e1da172d
Steve Chesnowitz <steve@chesnowitz.com>
2018-03-15 19:50:10 -0400

    alert working

---
+++ config/initializers/pusher.rb
@@ -0,0 +1,8 @@
+require 'pusher'
+
+Pusher.app_id = '470346'
+Pusher.key = '3bd6868fd1034baf277c'
+Pusher.secret = 'd784cd5636e5d256adcb'
+Pusher.cluster = 'us2'
+Pusher.logger = Rails.logger
+Pusher.encrypted = true 
